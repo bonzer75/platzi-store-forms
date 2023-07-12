7a02ae9a242c34bae1cc8df17f99d326f17addaf
bonzer75 <haroldv@tuta.io>
2023-07-12 02:03:56 +0000

    class 13

--- src/app/admin/components/basic-form/basic-form.component.ts
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-form',
@@ -8,34 +8,92 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class BasicFormComponent implements OnInit {
 
-  nameField = new FormControl('', [
-    Validators.required,
-    Validators.minLength(3)
-  ]);
-  emailField = new FormControl('');
-  phoneField = new FormControl('');
-  colorField = new FormControl('#000000');
-  dateField = new FormControl('');
-  ageField = new FormControl(12);
+  form: FormGroup
 
-  categoryField = new FormControl('');
-  tagField = new FormControl('');
+  constructor(
+    private builder: FormBuilder
+  ) { 
+    this.buildForm()
+  }
 
-  agreeField = new FormControl(false);
-  genderField = new FormControl('');
-  zoneField = new FormControl('');
-
-  constructor() { }
+  private buildForm() {
+    this.form = this.builder.group({
+      name: ["", Validators.required, Validators.maxLength(10)],
+      email: [''],
+      phone: [''],
+      color: ['#000000'],
+      date: [''],
+      age: [18],
+      category: [''],
+      tag: [''],
+      agree: [false],
+      gender: [''],
+      zone: [''],
+    }); 
+  }
 
   ngOnInit(): void {
-    this.nameField.valueChanges
-    .subscribe(value => {
-      console.log(value);
-    });
+    //
   }
 
   getNameValue() {
     console.log(this.nameField.value);
+  }
+
+  save(event) {
+    console.log(this.form.value);
+  }
+
+  get nameField() {
+    return this.form.get('name');
+  }
+
+  get isNameFieldValid() {
+    return this.nameField.touched && this.nameField.valid;
+  }
+
+  get isNameFieldInvalid() {
+    return this.nameField.touched && this.nameField.invalid;
+  }
+
+  get emailField() {
+    return this.form.get('email');
+  }
+
+  get phoneField() {
+    return this.form.get('phone');
+  }
+
+  get colorField() {
+    return this.form.get('color');
+  }
+
+  get dateField() {
+    return this.form.get('date');
+  }
+
+  get ageField() {
+    return this.form.get('age');
+  }
+
+  get categoryField() {
+    return this.form.get('category');
+  }
+
+  get tagField() {
+    return this.form.get('tag');
+  }
+
+  get agreeField() {
+    return this.form.get('agree');
+  }
+
+  get genderField() {
+    return this.form.get('gender');
+  }
+
+  get zoneField() {
+    return this.form.get('zone');
   }
 
 }
